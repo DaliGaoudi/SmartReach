ca30807e91b73274236e4b9e4e380a10d26b6988
Mohamed Ali Gaoudi <dali.gaoudi@gmail.com>
2025-08-19 00:37:07 +0200

    redirect

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,10 +1,17 @@
+
 import { getProducts, getSubscription } from "@/lib/supabase/db";
 import Pricing from "./pricing-client";
+import { redirect } from "next/navigation";
+
+const isSoftLaunch = process.env.SOFT_LAUNCH === "true";
 
 export default async function Page() {
-    const [{ subscription, user }, products] = await Promise.all([getSubscription(), getProducts()])
-
+    if (isSoftLaunch) {
+        // During soft launch, redirect all users to the waitlist page
+        redirect("/waitlist");
+    }
+    const [{ subscription, user }, products] = await Promise.all([getSubscription(), getProducts()]);
     return (
         <Pricing user={user} products={products} subscription={subscription} />
-    )
-} 
+    );
+}
